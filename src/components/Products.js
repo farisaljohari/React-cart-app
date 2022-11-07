7f9586515f52423db10935fe5786f77006508195
farisaljohari <farisaljohari@gmail.com>
2022-11-07 19:00:17 +0300

    third fixed

--- src/components/Products.js
+++ src/components/Products.js
@@ -17,6 +17,7 @@ function Products(){
         return () => {
             // cleanup
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return(
         <Container className='py-5'>
